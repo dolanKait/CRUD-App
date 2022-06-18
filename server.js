c4ff4dc82b2742f8955e5207c9f15cfb99d5e442
kaitlyn dolan <98899869+dolanKait@users.noreply.github.com>
2022-06-17 19:31:22 -0500

    Updated index/server file
    Decided to make my first CRUD app dedicated to the iconic reality television franchise: Real Housewives

--- server.js
+++ server.js
@@ -1,37 +1,38 @@
-// express is enabled to handle http connection, but it doesn't handle information that comes from a form, so middleware (bodyParser)  is used to gain functionality. 
-
-const express = require ('express')
-const app= express()
-const bodyParser= require ('body-parser')  
-const { MongoClient } = require ('mongodb').MongoClient
-const PORT = 8000
-
-// express listen() method w/ template literal 
-app.listen(PORT,  () => {
-    console.log(`Server is listening on ${PORT}`)
-})
 
 
-app.use(bodyParser.urlencoded({extended: true}))
-
-// CRUD handlers
+const express = require('express');
+const app=express();
+const port=3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html")
+    res.sendFile(__dirname + '/index.html')
 })
 
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
+app.listen(port,  () => {
+    console.log(`Server is listening on ${port}`)
 })
 
 
-// // mongoclient has a connect method tied to the particular db via username & pw in the connection string that's generated 
 
-MongoClient.connect(connectionString, (err, client) => {
-    mongodb+srv//dolankait:<superfreak>@cluster0.itf5l.mongodb.net/star-wars?retryWrites=true&w=majority
-} ) 
 
-MongoClient.connect(connectionString, (err, client) => {
-    if(err) return console.log(err)
-    console.log('Connected to Database')
-})
+// app.use(bodyParser.urlencoded({extended: true}))
+
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + "/index.html")
+// })
+
+// app.post('/quotes', (req, res) => {
+//     console.log(req.body)
+// })
+
+
+// // // mongoclient has a connect method tied to the particular db via username & pw in the connection string that's generated 
+
+// MongoClient.connect(connectionString, (err, client) => {
+//     mongodb+srv//dolankait:<superfreak>@cluster0.itf5l.mongodb.net/star-wars?retryWrites=true&w=majority
+// } ) 
+
+// MongoClient.connect(connectionString, (err, _client) => {
+//     if(err) return console.log(err)
+//     console.log('Connected to Database')
+// })
